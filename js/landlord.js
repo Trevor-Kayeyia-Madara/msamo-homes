db81467d201bfe6c0fce61c4decc38a82c41a1e0
Trevor-Kayeyia-Madara <trevor.madara@student.moringaschool.com>
2024-07-04 17:12:23 +0300

    I Have removed loading to dashboard

--- js/landlord.js
+++ js/landlord.js
@@ -19,23 +19,3 @@ document.getElementById('modify-property').addEventListener('click', function(ev
     // Navigate to property.html
     window.location.href = 'property.html';
 });
-// Simulate loading process
-window.addEventListener('load', () => {
-    setTimeout(() => {
-        const loader = document.getElementById('loader');
-        const loginForm = document.getElementById('dashboard-container');
-   // Fade out loader
-   loader.style.transition = 'opacity 0.25s'; // Reduced transition duration
-   loader.style.opacity = '0';
-   setTimeout(() => {
-       loader.style.display = 'none';
-       // Fade in login form
-       loginForm.style.display = 'flex';
-       setTimeout(() => {
-           loginForm.style.transition = 'opacity 0.25s'; // Reduced transition duration
-           loginForm.classList.add('show');
-            }, 10); // Slight delay to trigger transition
-        }, 250); // Match this with the transition duration of loader
-    }, 500); // Reduced delay for loading spinner
-});
-
