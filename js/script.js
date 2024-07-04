2c8fc8460d63e1d6f02b70d2f578aa7299a26325
Trevor-Kayeyia-Madara <trevor.madara@student.moringaschool.com>
2024-07-04 17:11:05 +0300

    I have removed loading to login

--- js/script.js
+++ js/script.js
@@ -1,28 +1,13 @@
-// Simulate loading process
-window.addEventListener('load', () => {
-    setTimeout(() => {
-        const loader = document.getElementById('loader');
-        const loginForm = document.getElementById('login-form');
-
-        // Fade out loader
-        loader.style.transition = 'opacity 0.25s'; // Reduced transition duration
-        loader.style.opacity = '0';
-        setTimeout(() => {
-            loader.style.display = 'none';
-            // Fade in login form
-            loginForm.style.display = 'flex';
-            setTimeout(() => {
-                loginForm.style.transition = 'opacity 0.25s'; // Reduced transition duration
-                loginForm.classList.add('show');
-            }, 10); // Slight delay to trigger transition
-        }, 250); // Match this with the transition duration of loader
-    }, 500); // Reduced delay for loading spinner
+document.addEventListener('DOMContentLoaded', () => {
+    const loginForm = document.getElementById('login-form');
+    loginForm.style.display = 'flex';
+    loginForm.classList.add('show');
 });
 
 // Handle form submission
 document.getElementById('loginForm').addEventListener('submit', (event) => {
     event.preventDefault(); // Prevent the default form submission
-    window.location.href = 'dashboard.html'; // Redirect to the  dashboard
+    window.location.href = 'dashboard.html'; // Redirect to the dashboard
 });
 
 function selectUserType(userType) {
