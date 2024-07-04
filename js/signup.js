d5f26df21cac35beef346d5e5c5bce658d60fe5b
Trevor-Kayeyia-Madara <trevor.madara@student.moringaschool.com>
2024-07-04 08:48:39 +0300

    Reduced even more speed

--- js/signup.js
+++ js/signup.js
@@ -2,20 +2,23 @@
 window.addEventListener('load', () => {
     setTimeout(() => {
         const loader = document.getElementById('loader');
-        const loginForm = document.getElementById('signup-form');
-        
+        const loginForm = document.getElementById('tenant');
+
         // Fade out loader
+        loader.style.transition = 'opacity 0.25s'; // Reduced transition duration
         loader.style.opacity = '0';
         setTimeout(() => {
             loader.style.display = 'none';
             // Fade in login form
             loginForm.style.display = 'flex';
             setTimeout(() => {
+                loginForm.style.transition = 'opacity 0.25s'; // Reduced transition duration
                 loginForm.classList.add('show');
-            }, 20); // Slight delay to trigger transition
-        }, 500); // Match this with the transition duration of loader
-    }, 1000); // 3 seconds delay for loading spinner
+            }, 10); // Slight delay to trigger transition
+        }, 250); // Match this with the transition duration of loader
+    }, 500); // Reduced delay for loading spinner
 });
+
 document.getElementById('signupForm').addEventListener('submit', (event) => {
     event.preventDefault(); // Prevent the default form submission
     window.location.href = 'signin.html'; // Redirect to the landlord dashboard
